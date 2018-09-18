436ca9db1c1648ddf03924f2240b473e280145cc
r <r@rs-MacBook-Pro.local>
2018-09-18 11:11:30 -0300

    [add] tests

--- src/test/Main.test.js
+++ src/test/Main.test.js
@@ -1,20 +1,13 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  Router,
-} from 'react-router';
-import App from '../containers/App';
+import Main from '../containers/Main.container';
 
 import createBrowserHistory from 'history/createBrowserHistory';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
   const history = createBrowserHistory();
-  ReactDOM.render(
-    <Router history={history}>
-      <App />
-    </Router>,
-    div,
-  );
+  const div = document.createElement('div');
+  ReactDOM.render(<Main history={history} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
